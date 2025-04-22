d46b63c5b9f9a5dc2ef38cdbbbdf1984ae7bb260
alemoraru <alexradumoraru10@gmail.com>
2025-04-22 17:56:15 +0200

    Enhance first blog post with code snippet and improve blog post layout

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,6 +3,7 @@ import posts from '@/data/blog'
 import path from 'path'
 import fs from 'fs'
 import {compileMDX} from 'next-mdx-remote/rsc'
+import Link from 'next/link'
 
 export default async function BlogPostPage({params}: { params: { slug: string } }) {
     const {slug} = await params // Await params before destructuring
@@ -20,10 +21,19 @@ export default async function BlogPostPage({params}: { params: { slug: string } 
     const {content} = await compileMDX({source: mdxSource})
 
     return (
-        <article className="mx-auto px-4">
-            <h1>{post.title}</h1>
-            <p className="text-gray-500">{post.date}</p>
-            {content}
+        <article className="mx-auto px-4 max-w-3xl flex flex-col items-center justify-center text-left">
+            <Link
+                href="/blog"
+                className="mb-8 text-blue-500 hover:text-blue-700 transition-all flex items-center gap-2"
+            >
+                <span className="inline-block transform transition-transform group-hover:-translate-x-1">
+                    ←
+                </span>
+                Back to blogs
+            </Link>
+            <h1 className="text-3xl font-bold text-center mb-4">{post.title}</h1>
+            <p className="text-gray-500 text-center mb-8">{post.date}</p>
+            <div className="prose dark:prose-invert">{content}</div>
         </article>
     )
 }
