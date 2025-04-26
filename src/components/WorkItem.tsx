b6c295e7b25703e72a16949a4adfe4d88fc2836f
alemoraru <alexradumoraru10@gmail.com>
2025-04-26 22:23:48 +0200

    Improve responsiveness of work items

--- src/components/WorkItem.tsx
+++ src/components/WorkItem.tsx
@@ -26,15 +26,17 @@ export default function WorkItem({slug, company, title, start, end, description,
                 <h3 className="text-xl font-semibold">{title} @ {company}</h3>
 
                 {/* Duration and Locations */}
-                <div className="mt-2 text-gray-500 flex items-center">
-                    <FaCalendarAlt className="w-4 h-4 mr-1"/>
-                    <span>{start} – {end}</span>
+                <div className="mt-2 text-gray-500 flex flex-col sm:flex-row sm:items-center">
+                    <div className="flex items-center">
+                        <FaCalendarAlt className="w-4 h-4 mr-1"/>
+                        <span>{start} – {end}</span>
+                    </div>
                     {locations && locations.length > 0 && (
-                        <>
-                            <span className="mx-2">|</span>
+                        <div className="flex items-center mt-1 sm:mt-0 sm:ml-2">
+                            <span className="hidden sm:inline mx-2">|</span>
                             <FaMapMarkerAlt className="w-4 h-4 mr-1"/>
                             <span>{locations.join(', ')}</span>
-                        </>
+                        </div>
                     )}
                 </div>
 
