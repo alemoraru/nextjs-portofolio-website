d3b3bddb0ad59c8a4e5f8958a93e1c1924b29234
alemoraru <alexradumoraru10@gmail.com>
2025-05-31 12:30:47 +0200

    Import remark-gfm

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,21 @@
 import type {NextConfig} from "next";
 import createMDX from '@next/mdx'
-
-const nextConfig: NextConfig = {
-    /* config options here */
-    reactStrictMode: true,
-    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx']
-};
+import remarkGfm from "remark-gfm";
 
 // Create MDX configuration
 const withMDX = createMDX({
     extension: /\.mdx$/,
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: []
+    }
 })
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    reactStrictMode: true,
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx']
+};
 
 // Merge MDX config with Next.js config
 export default withMDX(nextConfig)
