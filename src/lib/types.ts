e235bca476b0b601bda117ccae3e3bd90bceacdb
alemoraru <alexradumoraru10@gmail.com>
2025-09-12 16:30:30 +0200

    Add projects page URL params

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -15,6 +15,19 @@ export interface BlogPostProps {
 }
 
 /**
+ * @description This interface defines the structure (i.e., contents) of a project card.
+ */
+export interface ProjectProps {
+    slug: string;
+    title: string;
+    image: string;
+    description: string;
+    startDate: string;
+    endDate: string;
+    techStack: string[];
+}
+
+/**
  * @description This type is used to define the params for a dynamic tag route in Next.js.
  */
 export type tagPageParams = Promise<{ tag: string }>;
