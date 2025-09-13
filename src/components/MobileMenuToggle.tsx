bc1b7cb5bd624646116b1746e71d8691220aa38a
alemoraru <alexradumoraru10@gmail.com>
2025-09-13 14:28:47 +0200

    Improve mobile header icons

--- src/components/MobileMenuToggle.tsx
+++ src/components/MobileMenuToggle.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaXmark, FaBars} from "react-icons/fa6";
 
 /**
  * A functional component that renders a mobile menu toggle button with an icon.
@@ -11,17 +11,15 @@ export default function MobileMenuToggle({isOpen, onToggleAction}: {
 }) {
     return (
         <button
-            className="md:hidden p-2 rounded-full hover:bg-gray-800 transition-colors"
+            className="md:hidden p-2 rounded-full hover:bg-gray-800 transition-colors mt-1"
             onClick={onToggleAction}
             aria-label="Toggle Navigation Menu"
         >
-      <span
-          className={`inline-block transform transition-transform duration-300 ease-in-out ${
-              isOpen ? 'rotate-90' : 'rotate-0'
-          }`}
-      >
-        {isOpen ? <FaTimes/> : <FaBars/>}
-      </span>
+        <span className={`inline-block transform transition-transform duration-300 ease-in-out ${
+            isOpen ? 'rotate-90' : 'rotate-0'}`}
+        >
+            {isOpen ? <FaXmark className="w-5 h-5"/> : <FaBars className="w-5 h-5"/>}
+        </span>
         </button>
     )
 }
