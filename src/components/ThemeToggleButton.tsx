df1fadb7dbe9373778fc4ad38304f8c12d8a46e5
alemoraru <alexradumoraru10@gmail.com>
2025-12-31 20:07:15 +0100

    feat: improve animation for theme toggle button

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -48,8 +48,9 @@ export default function ThemeToggleButton() {
           exit={{ rotate: 90, opacity: 0, scale: 0.6 }}
           transition={{
             type: "spring",
-            stiffness: 300,
-            damping: 20,
+            stiffness: 500,
+            damping: 25,
+            mass: 0.5,
           }}
           className="absolute pointer-events-none"
         >
