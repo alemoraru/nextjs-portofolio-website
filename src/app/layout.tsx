ef57195896e4a233ec01abe8d0fd797e0b062738
alemoraru <alexradumoraru10@gmail.com>
2026-07-04 14:28:21 +0200

    feat: add accent theme color switcher

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Gabarito } from "next/font/google"
 import { ThemeProvider } from "next-themes"
 import React, { ReactNode } from "react"
+import { AccentThemeProvider } from "@/components/AccentThemeProvider"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import { siteMetadata } from "@/data/metadata"
@@ -58,23 +59,25 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         suppressHydrationWarning
       >
         <ThemeProvider attribute="class" defaultTheme="system">
-          {/* Dot Background Layer */}
-          <div
-            className={cn(
-              "fixed inset-0 -z-10",
-              "bg-[radial-gradient(circle,#d1d5db_1px,transparent_1px)]",
-              "dark:bg-[radial-gradient(circle,#3f3f46_1px,transparent_1px)]",
-              "bg-size-[30px_30px]",
-              "mask-[radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"
-            )}
-          />
-          <Header />
-          <main className="grow container mx-auto px-4 py-6">
-            {children}
-            <Analytics />
-            <SpeedInsights />
-          </main>
-          <Footer />
+          <AccentThemeProvider>
+            {/* Dot Background Layer */}
+            <div
+              className={cn(
+                "fixed inset-0 -z-10",
+                "bg-[radial-gradient(circle,#d1d5db_1px,transparent_1px)]",
+                "dark:bg-[radial-gradient(circle,#3f3f46_1px,transparent_1px)]",
+                "bg-size-[30px_30px]",
+                "mask-[radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"
+              )}
+            />
+            <Header />
+            <main className="grow container mx-auto px-4 py-6">
+              {children}
+              <Analytics />
+              <SpeedInsights />
+            </main>
+            <Footer />
+          </AccentThemeProvider>
         </ThemeProvider>
       </body>
     </html>
